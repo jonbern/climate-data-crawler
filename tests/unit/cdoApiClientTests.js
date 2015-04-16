355e55aec6428c28924c224025c88f2035615891
Jon Kristian Bernhardsen <jonkristianbernhardsen@Jons-MacBook-Pro.local>
2015-04-16 21:48:54 +0200

    improved unit test structure and refactoring of tests

--- tests/unit/cdoApiClientTests.js
+++ tests/unit/cdoApiClientTests.js
@@ -74,27 +74,7 @@ describe('CdoClient', function(){
   };
 
   describe('#invoke', function() {
-
-    it('path should use supplied offset value', function(){
-      sinon.stub(httpClient, 'request');
-      var client = getInstance();
-
-      var expected1 = '/cdo-web/api/v2/data?datasetid=GHCND&locationid=CITY:AS000002&startdate=2012-01-01&enddate=2012-06-01&datatypeid=PRCP&limit=1000&offset=1';
-      var expected2 = '/cdo-web/api/v2/data?datasetid=GHCND&locationid=CITY:AS000002&startdate=2012-01-01&enddate=2012-06-01&datatypeid=PRCP&limit=1000&offset=7';
-
-      // act
-      client.query(1);
-      client.query(7);
-
-      // assert
-      httpClient.request.getCall(0).args[0].path
-        .should.equal(expected1);
-
-      httpClient.request.getCall(1).args[0].path
-        .should.equal(expected2);
-    });
-
-    it('path should use configuration\'s dataset value', function() {
+    it('should use dataset from constructor argument', function() {
       // arrange
       dataset = "mydataset";
 
@@ -118,7 +98,7 @@ describe('CdoClient', function(){
         .should.equal(expected);
     });
 
-    it('path should use configuration\'s locationid value', function() {
+    it('should use locationid from constructor argument', function() {
       // arrange
       locationId = "AO:MYCITY";
 
@@ -142,7 +122,7 @@ describe('CdoClient', function(){
         .should.equal(expected);
     });
 
-    it('should use configuration\'s enddate value', function() {
+    it('should use enddate from constructor parameter', function() {
       // arrange
       endDate = "23-10-1983";
 
@@ -166,7 +146,7 @@ describe('CdoClient', function(){
         .should.equal(expected);
     });
 
-    it('should use configuration\'s startdate value', function() {
+    it('should use startdate from constructor argument', function() {
       // arrange
       startDate = "23-10-1983";
 
@@ -190,7 +170,7 @@ describe('CdoClient', function(){
         .should.equal(expected);
     });
 
-    it('should make http request', function(){
+    it('should use httpClient to make http request', function(){
       // arrange
       sinon.stub(httpClient, 'request');
       var client = getInstance();
@@ -218,187 +198,7 @@ describe('CdoClient', function(){
       logger.error.called.should.be.true;
     });
 
-    it('should make another api call if there is still data left to query', function(){
-      // arrange
-      var apiResult = {
-        'metadata': {
-          'resultset': {
-            'limit': 25,
-            'count': 100,
-            'offset': 1
-          }
-        }
-      };
-      var counter = 0;
-      var stubRequest = function(options, successCallback, errorCallback) {
-        var resultset = apiResult.metadata.resultset;
-        resultset.offset = (counter * resultset.limit) + 1;
-        counter++;
-        successCallback(JSON.stringify(apiResult));
-      };
-      sinon.stub(httpClient, 'request', stubRequest);
-
-      var client = getInstance();
-      sinon.spy(client, 'query');
-
-      // act
-      client.query();
-
-      // assert
-      var queryPath = '/cdo-web/api/v2/data?datasetid=GHCND&locationid=CITY:AS000002&startdate=2012-01-01&enddate=2012-06-01&datatypeid=PRCP&limit=1000';
-
-      httpClient.request.getCall(0).args[0].path
-        .should.equal(queryPath + '&offset=1');
-
-      httpClient.request.getCall(1).args[0].path
-        .should.equal(queryPath + '&offset=26');
-
-      httpClient.request.getCall(2).args[0].path
-        .should.equal(queryPath + '&offset=51');
-
-      httpClient.request.getCall(3).args[0].path
-        .should.equal(queryPath + '&offset=76');
-    });
-
-    it('should make delayed api calls in case there is more data to query', function(){
-      // arrange
-      var apiResult = {
-        'metadata': {
-          'resultset': {
-            'limit': 25,
-            'count': 50,
-            'offset': 1
-          }
-        }
-      };
-      var counter = 0;
-      var stubRequest = function(options, successCallback, errorCallback) {
-        var resultset = apiResult.metadata.resultset;
-        resultset.offset = (counter * resultset.limit) + 1;
-        counter++;
-        successCallback(JSON.stringify(apiResult));
-      };
-
-      sinon.stub(httpClient, 'request', stubRequest);
-      var client = getInstance();
-
-      // act
-      client.query();
-
-      // assert
-      timer.setTimeout.getCall(0).args[1].should.be.greaterThan(1000);
-
-    });
-
-    it('should emit done event when there is no more data to query', function(){
-      // arrange
-      var apiResult = {
-        'metadata': {
-          'resultset': {
-            'limit': 25,
-            'count': 15,
-            'offset': 1
-          }
-        }
-      };
-      var stubRequest = function(options, successCallback, errorCallback) {
-        successCallback(JSON.stringify(apiResult));
-      };
-
-      sinon.stub(httpClient, 'request', stubRequest);
-      sinon.spy(eventEmitter, 'emit');
-
-      var client = getInstance();
-
-      // act
-      client.query();
-
-      // assert
-      eventEmitter.emit.calledWith('done').should.be.true;
-    });
-
-    it('done event should contain result of query', function(){
-      // arrange
-      var simulatedApiResult = {
-        "results": [
-          {
-            "id": "CITY:AE000002",
-            "name": "Ajman, AE",
-            "datacoverage": 0.6862,
-            "mindate": "1944-03-01",
-            "maxdate": "2015-01-01"
-          },
-          {
-            "id": "CITY:AE000003",
-            "name": "Dubai, AE",
-            "datacoverage": 0.6862,
-            "mindate": "1944-03-01",
-            "maxdate": "2015-01-01"
-          }
-        ],
-        'metadata': {
-          'resultset': {
-            'limit': 25,
-            'count': 2,
-            'offset': 1
-          }
-        }
-      };
-      var stubRequest = function(options, successCallback, errorCallback) {
-        successCallback(JSON.stringify(simulatedApiResult));
-      };
-      sinon.stub(httpClient, 'request', stubRequest);
-      var spy = sinon.spy(eventEmitter, 'emit');
-
-      var actualResults = null;
-      var resultCallback = function(result){
-        actualResults = result;
-      };
-
-      var client = getInstance();
-
-      // act
-      client.query(1, resultCallback);
-
-      // assert
-      assert.equal(spy.calledWith('done', simulatedApiResult.results), true);
-      assert.equal(JSON.stringify(actualResults),
-        JSON.stringify(simulatedApiResult.results));
-    });
-
-    it('should read api-token from file', function(){
-      // arrange
-      var client = getInstance();
-
-      // act
-      client.query();
-
-      // assert
-      var call = fs.readFileSync.getCall(0);
-      call.args[0].should.equal('apitoken.txt');
-      call.args[1].should.equal('utf8');
-    });
-
-    it('should write helpful message when there is no api-token file', function(){
-      // arrange
-      fs.readFileSync.restore();
-      sinon.stub(fs, 'readFileSync', function(filepath, encoding){
-        throw new Error('ENOENT, no such file or directory');
-      });
-
-      var expectedMessage = "Please make sure there is an apitoken.txt file (containing your API token) in the executing directory.";
-
-      var client = getInstance();
-
-      // act
-      client.query();
-
-      // assert
-      logger.info.called.should.be.true;
-      logger.info.calledWith(expectedMessage).should.be.true;
-    });
-
-    it('should query http client using correct options', function(){
+    it('should use httpClient using correct options', function(){
       // arrange
       var stubRequest = function(options, successCallback, errorCallback) {
         successCallback(JSON.stringify({}));
@@ -423,22 +223,255 @@ describe('CdoClient', function(){
         .should.be.true;
     });
 
-    it('should handle empty result {} from api and emit done', function(){
-      // arrange
-      var apiResult = {};
-       var stubRequest = function(options, successCallback, errorCallback) {
-        successCallback(JSON.stringify(apiResult));
-      };
-      sinon.stub(httpClient, 'request', stubRequest);
-      var spy = sinon.spy(eventEmitter, 'emit');
+    describe("data paging functionality", function(){
+      it('should make another httpRequest against api if there is more data to query (data paging)', function(){
+        // arrange
+        var apiResult = {
+          'metadata': {
+            'resultset': {
+              'limit': 25,
+              'count': 100,
+              'offset': 1
+            }
+          }
+        };
+        var counter = 0;
+        var stubRequest = function(options, successCallback, errorCallback) {
+          var resultset = apiResult.metadata.resultset;
+          resultset.offset = (counter * resultset.limit) + 1;
+          counter++;
+          successCallback(JSON.stringify(apiResult));
+        };
+        sinon.stub(httpClient, 'request', stubRequest);
 
-      var client = getInstance();
+        var client = getInstance();
+        sinon.spy(client, 'query');
 
-      // act
-      client.query();
+        // act
+        client.query();
 
-      // assert
-      spy.calledWith('done', null).should.be.true;
+        // assert
+        var queryPath = '/cdo-web/api/v2/data?datasetid=GHCND&locationid=CITY:AS000002&startdate=2012-01-01&enddate=2012-06-01&datatypeid=PRCP&limit=1000';
+
+        httpClient.request.getCall(0).args[0].path
+          .should.equal(queryPath + '&offset=1');
+
+        httpClient.request.getCall(1).args[0].path
+          .should.equal(queryPath + '&offset=26');
+
+        httpClient.request.getCall(2).args[0].path
+          .should.equal(queryPath + '&offset=51');
+
+        httpClient.request.getCall(3).args[0].path
+          .should.equal(queryPath + '&offset=76');
+      });
+
+      it('should make delayed api calls in case there is more data to query', function(){
+        // arrange
+        var apiResult = {
+          'metadata': {
+            'resultset': {
+              'limit': 25,
+              'count': 50,
+              'offset': 1
+            }
+          }
+        };
+        var counter = 0;
+        var stubRequest = function(options, successCallback, errorCallback) {
+          var resultset = apiResult.metadata.resultset;
+          resultset.offset = (counter * resultset.limit) + 1;
+          counter++;
+          successCallback(JSON.stringify(apiResult));
+        };
+
+        sinon.stub(httpClient, 'request', stubRequest);
+        var client = getInstance();
+
+        // act
+        client.query();
+
+        // assert
+        timer.setTimeout.getCall(0).args[1].should.be.greaterThan(1000);
+
+      });
+    });
+
+    describe('emit event', function(){
+
+      it('should emit done when there is no more data to query', function(){
+        // arrange
+        var apiResult = {
+          'metadata': {
+            'resultset': {
+              'limit': 25,
+              'count': 15,
+              'offset': 1
+            }
+          }
+        };
+        var stubRequest = function(options, successCallback, errorCallback) {
+          successCallback(JSON.stringify(apiResult));
+        };
+
+        sinon.stub(httpClient, 'request', stubRequest);
+        sinon.spy(eventEmitter, 'emit');
+
+        var client = getInstance();
+
+        // act
+        client.query();
+
+        // assert
+        assert.equal(eventEmitter.emit.calledWith('done'), true);
+      });
+
+      it('event should contain api results', function(){
+        // arrange
+        var expectedResults = {
+          "results": [
+            {
+              "id": "CITY:AE000002",
+              "name": "Ajman, AE",
+              "datacoverage": 0.6862,
+              "mindate": "1944-03-01",
+              "maxdate": "2015-01-01"
+            },
+            {
+              "id": "CITY:AE000003",
+              "name": "Dubai, AE",
+              "datacoverage": 0.6862,
+              "mindate": "1944-03-01",
+              "maxdate": "2015-01-01"
+            }
+          ],
+          'metadata': {
+            'resultset': {
+              'limit': 25,
+              'count': 2,
+              'offset': 1
+            }
+          }
+        };
+
+        var stubRequest = function(options, successCallback, errorCallback) {
+          successCallback(JSON.stringify(expectedResults));
+        };
+        sinon.stub(httpClient, 'request', stubRequest);
+        var spy = sinon.spy(eventEmitter, 'emit');
+
+        var client = getInstance();
+
+        // act
+        client.query(0);
+
+        // assert
+        assert.equal(spy.calledWith('done', expectedResults.results), true);
+      });
+
+      it('should handle empty result {} from api and emit done', function(){
+        // arrange
+        var apiResult = {};
+        var stubRequest = function(options, successCallback, errorCallback) {
+          successCallback(JSON.stringify(apiResult));
+        };
+        sinon.stub(httpClient, 'request', stubRequest);
+        var spy = sinon.spy(eventEmitter, 'emit');
+
+        var client = getInstance();
+
+        // act
+        client.query();
+
+        // assert
+        spy.calledWith('done', null).should.be.true;
+      });
+
+    });
+
+    describe('result callback', function(){
+      it('done event should contain result of query', function(){
+        // arrange
+        var expectedResults = {
+          "results": [
+            {
+              "id": "CITY:AE000002",
+              "name": "Ajman, AE",
+              "datacoverage": 0.6862,
+              "mindate": "1944-03-01",
+              "maxdate": "2015-01-01"
+            },
+            {
+              "id": "CITY:AE000003",
+              "name": "Dubai, AE",
+              "datacoverage": 0.6862,
+              "mindate": "1944-03-01",
+              "maxdate": "2015-01-01"
+            }
+          ],
+          'metadata': {
+            'resultset': {
+              'limit': 25,
+              'count': 2,
+              'offset': 1
+            }
+          }
+        };
+
+        var stubRequest = function(options, successCallback, errorCallback) {
+          successCallback(JSON.stringify(expectedResults));
+        };
+        sinon.stub(httpClient, 'request', stubRequest);
+
+        var actualResults = null;
+        var resultCallback = function(result){
+          actualResults = result;
+        };
+
+        var client = getInstance();
+
+        // act
+        //client.query(0, resultCallback);
+        client.query(0, resultCallback);
+
+        // assert
+        assert.equal(JSON.stringify(actualResults),
+          JSON.stringify(expectedResults.results));
+      });
+    });
+
+    describe('api token', function(){
+      it('should read api-token from file', function(){
+        // arrange
+        var client = getInstance();
+
+        // act
+        client.query();
+
+        // assert
+        var call = fs.readFileSync.getCall(0);
+        call.args[0].should.equal('apitoken.txt');
+        call.args[1].should.equal('utf8');
+      });
+
+      it('should write helpful message when there is no api-token file', function(){
+        // arrange
+        fs.readFileSync.restore();
+        sinon.stub(fs, 'readFileSync', function(filepath, encoding){
+          throw new Error('ENOENT, no such file or directory');
+        });
+
+        var expectedMessage = "Please make sure there is an apitoken.txt file (containing your API token) in the executing directory.";
+
+        var client = getInstance();
+
+        // act
+        client.query();
+
+        // assert
+        assert.equal(logger.info.called, true);
+        assert.equal(logger.info.calledWith(expectedMessage), true);
+      });
     });
 
   });

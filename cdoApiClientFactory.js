66660b130b79c340d979ccbf52158ddcdb898553
Jon Kristian Bernhardsen <jon@Jons-MacBook-Pro.local>
2015-05-23 00:26:21 +0200

    Minor clean up

--- cdoApiClientFactory.js
+++ cdoApiClientFactory.js
@@ -1,11 +1,8 @@
 "use strict";
 var CdoApiClient = require('./cdoApiClient');
-var CdoApiClientFactory = require('./cdoApiClientFactory');
 
 exports.createInstance = function(
   locationid, dataset, datatypeid, startDate, endDate){
 
-
   return CdoApiClient.createInstance(locationid, dataset, datatypeid, startDate, endDate)
-
 };

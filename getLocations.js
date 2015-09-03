9c6b9f38a0d26234f22476990dab569a240acde0
Jon Kristian Bernhardsen <enlounge@gmail.com>
2015-09-03 23:39:51 +0200

    linting

--- getLocations.js
+++ getLocations.js
@@ -1,15 +1,15 @@
 var fs = require('fs');
-var cdoApiClient = require("./cdoApiClient")
+var cdoApiClient = require("./cdoApiClient");
 
 function getLocations() {
-  var queryPath = '/cdo-web/api/v2/locations?locationcategoryid=city&sortfield=name&limit=1000'
+  var queryPath = '/cdo-web/api/v2/locations?locationcategoryid=city&sortfield=name&limit=1000';
   var client = cdoApiClient.createInstance(queryPath);
 
   client.query(function(result) {
     fs.appendFileSync('./CITIES.json', JSON.stringify(result) + '\r\n');
     console.log('Done. CITIES.json written.');
   });
-};
+}
 
 getLocations();
 
